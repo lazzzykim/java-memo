847e2da7766c3dfe1b36052806d828c387a8048c
lazzzykim <33sunghyeon@gmail.com>
2024-08-26 17:31:10 +0900

    memo CRUD 구현

--- src/main/java/com/sparta/memo/controller/MemoController.java
+++ src/main/java/com/sparta/memo/controller/MemoController.java
@@ -28,11 +28,37 @@ public class MemoController {
         return memoResponseDto;
     }
 
-    @GetMapping("memos")
+    @GetMapping("/memos")
     public List<MemoResponseDto> getMemos() {
         List<MemoResponseDto> responseList = memoList.values().stream()
                 .map(MemoResponseDto::new).toList();
 
         return responseList;
+    }
+
+    @PutMapping("/memos/{id}")
+    public Long updateMemo(@PathVariable Long id,
+                           @RequestBody MemoRequestDto requestDto) {
+        if (memoList.containsKey(id)) {
+            // 메모 가져오기
+            Memo memo = memoList.get(id);
+            // 메모 수정
+            memo.update(requestDto);
+            return memo.getId();
+        } else {
+            throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다.");
+        }
+    }
+
+    @DeleteMapping("/memos/{id}")
+    public Long deleteMemo(@PathVariable Long id) {
+        // db에 id 존재하는지 확인
+        if (memoList.containsKey(id)) {
+            // 메모 삭제
+            memoList.remove(id);
+            return id;
+        } else {
+            throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다.");
+        }
     }
 }
